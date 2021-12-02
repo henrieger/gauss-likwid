76e18be39387d3e3318b78ad5074709ec76768c0
leonokida <le0n0kida@gmail.com>
2021-12-02 11:40:48 -0300

    [add] - Mensagens de status no script Shell

--- EP03/testes.sh
+++ EP03/testes.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #Executando comandos para poder utilizar o likwid
-echo "Frequência do processador configurada para modo:"
 echo "performance" | sudo tee /sys/devices/system/cpu/cpufreq/policy3/scaling_governor
 
 #Compilando programas
@@ -10,15 +9,17 @@ make geraSL
 clear
 
 #Rodando testes
-for i in 10 32 50 64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096
+for i in 10 32 50 #64 100 128 200 250 256 300 400 512 600 1000 1024 2000 2048 3000 4096
 do
+    echo "Processando sistema de dimensão $i"
     ./geraSL "$i" > sistemas.txt
     likwid-perfctr -C 3 -g L3 -m ./gaussJacobi-likwid sistemas.txt > l3_${i}.txt
     likwid-perfctr -C 3 -g L2CACHE -m ./gaussJacobi-likwid sistemas.txt > l2cache_${i}.txt
     likwid-perfctr -C 3 -g FLOPS_DP -m ./gaussJacobi-likwid sistemas.txt > flops_dp_${i}.txt
     likwid-perfctr -C 3 -g FLOPS_AVX -m ./gaussJacobi-likwid sistemas.txt > flops_avx_${i}.txt
+    clear
 done
-clear
+
 
 #Encerrando
 make purge
@@ -28,5 +29,6 @@ rm sistemas.txt
 #rm flops*.txt
 
 #Retornando para frequência original
+clear
 echo "Frequência do processador configurada para modo:"
 echo "powersave" | sudo tee /sys/devices/system/cpu/cpufreq/policy3/scaling_governor 
